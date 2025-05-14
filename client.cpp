8bad7c0c88c51126007ff0bacc5f707af03e8e06
TaljatH <taljat.heydarov@gmail.com>
2025-05-14 12:28:15 +0200

    05/14

--- client.cpp
+++ client.cpp
@@ -4,41 +4,119 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define SERVER_IP "127.0.0.1"
+
+void scenario1();
+void scenario2();
+void scenario3();
+void scenario4();
+
 int main() {
- int sock = socket(AF_INET, SOCK_STREAM, 0);
- if (sock < 0) {
- std::cerr << "Socket creation error" << std::endl;
- return 1;
- }
-
- struct sockaddr_in server_addr;
- memset(&server_addr, 0, sizeof(server_addr));
- server_addr.sin_family = AF_INET;
- server_addr.sin_port = htons(PORT);
-
- if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <=
-0) {
- std::cerr << "Invalid address or address not supported" <<
-std::endl;
- close(sock);
- return 1;
- }
-
- std::cout << "Connecting to server..." << std::endl;
- if (connect(sock, (struct sockaddr*)&server_addr,
-sizeof(server_addr)) < 0) {
- std::cerr << "Connection failed" << std::endl;
- close(sock);
- return 1;
+    std::cout << "Scenario 1: Normal data" << std::endl;
+    scenario1();
+    std::cout << "\nScenario 2: URGENT DATA REQUEST" << std::endl;
+    scenario2();
+    std::cout << "\nScenario 3: URGENT DATA without OOB" << std::endl;
+    scenario3();
+    std::cout << "\nScenario 4: Garbage" << std::endl;
+    scenario4();
+    return 0;
 }
 
-char buffer[BUFFER_SIZE] = {0};
-int valread = read(sock, buffer, BUFFER_SIZE);
+int create_and_connect_socket() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        std::cerr << "Socket creation failed\n";
+        return -1;
+    }
 
-std::cout << "Message from server: " << buffer << std::endl;
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
 
-close(sock);
-return 0;
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address\n";
+        close(sock);
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Connection failed\n";
+        close(sock);
+        return -1;
+    }
+
+    return sock;
+}
+
+void scenario1() {
+    int sock = create_and_connect_socket();
+    if (sock < 0) return;
+
+    const char* msg = "NORMAL_DATA:Hello";
+    send(sock, msg, strlen(msg), 0);
+    std::cout << "Sent: " << msg << std::endl;
+
+    char buffer[BUFFER_SIZE] = {};
+    int valread = recv(sock, buffer, BUFFER_SIZE, 0);
+    if (valread > 0)
+        std::cout << "Received: " << buffer << std::endl;
+
+    close(sock);
+}
+
+void scenario2() {
+    int sock = create_and_connect_socket();
+    if (sock < 0) return;
+
+    const char* request = "SEND_URGENT_REQUEST";
+    send(sock, request, strlen(request), 0);
+    std::cout << "Sent: " << request << std::endl;
+
+    const char* urgent_byte = "U";
+    send(sock, urgent_byte, 1, MSG_OOB);
+    std::cout << "Sent urgent byte: " << urgent_byte << std::endl;
+
+    char buffer[BUFFER_SIZE] = {};
+    int valread = recv(sock, buffer, BUFFER_SIZE, 0);
+    if (valread > 0)
+        std::cout << "Received: " << buffer << std::endl;
+
+    close(sock);
+}
+
+void scenario3() {
+    int sock = create_and_connect_socket();
+    if (sock < 0) return;
+
+    const char* request = "SEND_URGENT_REQUEST";
+    send(sock, request, strlen(request), 0);
+    std::cout << "Sent: " << request << std::endl;
+
+
+    char buffer[BUFFER_SIZE] = {};
+    int valread = recv(sock, buffer, BUFFER_SIZE, 0);
+    if (valread > 0)
+        std::cout << "Received: " << buffer << std::endl;
+
+    close(sock);
+}
+
+void scenario4() {
+    int sock = create_and_connect_socket();
+    if (sock < 0) return;
+
+    const char* msg = "TEST_GARBAGE";
+    send(sock, msg, strlen(msg), 0);
+    std::cout << "Sent: " << msg << std::endl;
+
+    char buffer[BUFFER_SIZE] = {};
+    int valread = recv(sock, buffer, BUFFER_SIZE, 0);
+    if (valread > 0)
+        std::cout << "Received: " << buffer << std::endl;
+
+    close(sock);
 }
--- server.cpp
+++ server.cpp
@@ -3,66 +3,99 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+
 #define PORT 8080
 #define BUFFER_SIZE 1024
+
+void handle_client(int client_socket) {
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Read normal data
+    int valread = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    if (valread <= 0) {
+        std::cerr << "Failed to receive data from client." << std::endl;
+        close(client_socket);
+        return;
+    }
+
+    std::string received(buffer);
+    std::cout << "Received: " << received << std::endl;
+
+    if (received.find("NORMAL_DATA:") == 0) {
+        std::string response = "SERVER_ACK:" + received.substr(strlen("NORMAL_DATA:"));
+        send(client_socket, response.c_str(), response.length(), 0);
+    }
+
+    else if (received == "SEND_URGENT_REQUEST") {
+        char oob_data;
+        int oob_result = recv(client_socket, &oob_data, 1, MSG_OOB);
+        if (oob_result == 1) {
+            std::string response = "SERVER_URGENT_ACK:";
+            response += oob_data;
+            send(client_socket, response.c_str(), response.length(), 0);
+        } else {
+            std::string response = "SERVER_NO_URGENT_DATA";
+            send(client_socket, response.c_str(), response.length(), 0);
+        }
+    }
+
+    else {
+        std::string response = "SERVER_UNKNOWN_COMMAND";
+        send(client_socket, response.c_str(), response.length(), 0);
+    }
+
+    close(client_socket);
+    std::cout << "Closed client connection.\n" << std::endl;
+}
+
 int main() {
- const char* welcome_message = "Hello from IoT NetworkProgramming Server!";
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
 
- int server_fd;
- if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
- std::cerr << "Socket creation failed" << std::endl;
- return 1;
- }
+   
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        std::cerr << "Socket creation failed" << std::endl;
+        return 1;
+    }
 
- int opt = 1;
- if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt,
-sizeof(opt))) {
- std::cerr << "setsockopt failed" << std::endl;
- close(server_fd);
- return 1;
- }
+    
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    memset(address.sin_zero, 0, sizeof(address.sin_zero));
 
- struct sockaddr_in address;
- address.sin_family = AF_INET;
- address.sin_addr.s_addr = INADDR_ANY;
- address.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) <= 0) {
+    std::cerr << "Invalid address" << std::endl;
+    return 1;}
 
- if (bind(server_fd, (struct sockaddr*)&address,
-sizeof(address)) < 0) {
- std::cerr << "Bind failed" << std::endl;
- close(server_fd);
- return 1;
- }
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "setsockopt failed" << std::endl;
+        close(server_fd);
+        return 1;
+    }
 
- if (listen(server_fd, 3) < 0) { // Allow up to 3 pending connections
- std::cerr << "Listen failed" << std::endl;
- close(server_fd);
- return 1;
- }
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Listen failed" << std::endl;
+        close(server_fd);
+        return 1;
+    }
 
- std::cout << "Server listening on port " << PORT << "..." <<
-std::endl;
+    std::cout << "Server listening on port " << PORT << std::endl;
 
- while (true) {
- struct sockaddr_in client_addr;
- socklen_t addrlen = sizeof(client_addr);
+    while (true) {
+        client_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen);
+        if (client_socket < 0) {
+            std::cerr << "Accept failed" << std::endl;
+            continue;
+        }
 
- int client_socket = accept(server_fd, (struct
-sockaddr*)&client_addr, &addrlen);
- if (client_socket < 0) {
- std::cerr << "Accept failed" << std::endl;
- continue;
- }
+        std::cout << "New client connected" << std::endl;
+        handle_client(client_socket);
+    }
 
- std::cout << "New client connected" << std::endl;
-
- send(client_socket, welcome_message,
-strlen(welcome_message), 0);
- std::cout << "Welcome message sent" << std::endl;
-
- close(client_socket);
- }
-
- close(server_fd);
- return 0;
+    close(server_fd);
+    return 0;
 }
